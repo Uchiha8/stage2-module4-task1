68cc730475e986e8790d6c1faa66f724888c293f
2110107 <najimovm11@gmail.com>
2023-09-16 18:49:38 +0500

    done

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,27 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+        try {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("h2database");
+            Class.forName(resourceBundle.getString("jdbc_driver"));
+            connection = DriverManager.getConnection(resourceBundle.getString("db_url"), resourceBundle.getString("user"), resourceBundle.getString("password"));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
 
